98574b2c33c81f1ee8512d2952f846cba52e693b
Adrian Ho <aho+bruteforce@03s.net>
2021-03-22 16:40:38 +0800

    Revised Homebrew bottle spec

--- Formula/encfs.rb
+++ Formula/encfs.rb
@@ -10,10 +10,10 @@ class Encfs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/encfs-1.9.5"
-    sha256 "a3ad5e221f05f624c74ff3015164ae6e2bac3a99537b3b0a9d8f0b546130abe7" => :big_sur
-    sha256 "41efd14389d04bf5d2a0bc957972b15b6e1ff511452c89749ddc68b6bca6e3e7" => :catalina
-    sha256 "aaf2aa568786ed055114cf54ee4b46acd66f74a7bc10715b446198164afa1871" => :mojave
-    sha256 "8f4401c6165c596ce0e9f4e526c355cd573581c64da5661e754afde9300e71b5" => :x86_64_linux
+    sha256 big_sur:      "a3ad5e221f05f624c74ff3015164ae6e2bac3a99537b3b0a9d8f0b546130abe7"
+    sha256 catalina:     "41efd14389d04bf5d2a0bc957972b15b6e1ff511452c89749ddc68b6bca6e3e7"
+    sha256 mojave:       "aaf2aa568786ed055114cf54ee4b46acd66f74a7bc10715b446198164afa1871"
+    sha256 x86_64_linux: "8f4401c6165c596ce0e9f4e526c355cd573581c64da5661e754afde9300e71b5"
   end
 
   depends_on "cmake" => :build
