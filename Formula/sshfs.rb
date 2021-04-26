3101b7a4b2c12f505805c214724503c82cb2c7af
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 01:18:04 +0800

    sshfs: update 2.10_1 bottle.

--- Formula/sshfs.rb
+++ Formula/sshfs.rb
@@ -10,8 +10,9 @@ class Sshfs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/sshfs-2.10"
-    sha256 cellar: :any, big_sur: "28ffe0e1b9e3b78d37630fa183678de2a9217b5399ef05add67828f9b2cf71f5"
-    sha256 cellar: :any, mojave:  "d647dc7757e960cb204a7da9147801eb9d6a449fc3237dfe5d8c7ddfab7af680"
+    sha256 cellar: :any, big_sur:  "28ffe0e1b9e3b78d37630fa183678de2a9217b5399ef05add67828f9b2cf71f5"
+    sha256 cellar: :any, catalina: "2d1e986c38364093529da58fd474c587dc63d8d90b5f889e8881049525ecc955"
+    sha256 cellar: :any, mojave:   "d647dc7757e960cb204a7da9147801eb9d6a449fc3237dfe5d8c7ddfab7af680"
   end
 
   depends_on "autoconf" => :build
