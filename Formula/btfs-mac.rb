2095c64fb57d2a0856c21349d6a67b91aab569d8
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 14:49:33 +0800

    btfs-mac: add 2.24 bottle.

---
+++ Formula/btfs-mac.rb
@@ -0,0 +1,37 @@
+require_relative "../require/macfuse"
+
+class BtfsMac < Formula
+  desc "BitTorrent filesystem based on FUSE"
+  homepage "https://github.com/johang/btfs"
+  url "https://github.com/johang/btfs/archive/v2.24.tar.gz"
+  sha256 "d71ddefe3c572e05362542a0d9fd0240d8d4e1578ace55a8b3245176e7fd8935"
+  license "GPL-3.0-only"
+  head "https://github.com/johang/btfs.git"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/btfs-mac-2.24"
+    sha256 cellar: :any, big_sur: "d92d62980a61112eaf05240b150122226cabaaeb9e52910d06a3db5475c9cf0d"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libtorrent-rasterbar"
+  depends_on MacfuseRequirement
+  depends_on :macos
+
+  def install
+    ENV.cxx11
+    inreplace "configure.ac", "fuse >= 2.8.0", "fuse >= 2.7.3"
+    system "autoreconf", "--force", "--install"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/btfs", "--help"
+  end
+end
