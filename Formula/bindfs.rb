4d0d4ee7c56d91b812970f3bc49ce88ebb1fe20c
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 11:43:29 +0800

    bindfs: update 1.15.1 bottle.

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -11,6 +11,7 @@ class Bindfs < Formula
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/bindfs-1.15.1"
     sha256 cellar: :any, big_sur:  "f940f18270f1319c5f27c755538949c87a3d282ab0cf2c7397067c3df67c6306"
     sha256 cellar: :any, catalina: "3d2d4117dc51e3c77a23baa2c35edefc3a2d184eb41ad8656840a7a43922ed07"
+    sha256 cellar: :any, mojave:   "e7fcdd329d514eff00daad7f368dd31c01aafb6c305237f167ea99d710dee73d"
   end
 
   head do
