7209b09c577f06984549084cd24aba3265ecf3c1
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 18:21:28 +0800

    afuse-mac: update 0.4.1 bottle.

--- Formula/afuse-mac.rb
+++ Formula/afuse-mac.rb
@@ -9,9 +9,7 @@ class AfuseMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/afuse-mac-0.4.1"
-    sha256 cellar: :any, big_sur:  "5793a602add435689cfcd384bd1e7627568765c2417a30014525eb0e40e71ef5"
-    sha256 cellar: :any, catalina: "2e3018f85263dffcd6ef7e906453ba274601219461bcda3c79d105465f55a175"
-    sha256 cellar: :any, mojave:   "d98cbc816c74cdb8c7423234b9542600cb5aa29581608e98ba34c9cbf4db7b86"
+    sha256 cellar: :any, big_sur: "21172cbf00c3fafeaa02e3f9d07f83eef6ed42cb8e7ae9afce32dbd468203eb1"
   end
 
   depends_on "pkg-config" => :build
