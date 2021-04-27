8a6357177352a138479756d52d04f692db6c737a
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 13:42:59 +0800

    gcsfuse: add 0.34.1 bottle.

---
+++ Formula/gcsfuse.rb
@@ -0,0 +1,35 @@
+require_relative "../require/macfuse"
+
+class Gcsfuse < Formula
+  desc "User-space file system for interacting with Google Cloud"
+  homepage "https://github.com/googlecloudplatform/gcsfuse"
+  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.34.1.tar.gz"
+  sha256 "740b3823a67b7431ac201a5fc715114ecc44ee43b913492c8b837474b22caf0c"
+  license "Apache-2.0"
+  head "https://github.com/GoogleCloudPlatform/gcsfuse.git"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/gcsfuse-0.34.1"
+    sha256 cellar: :any_skip_relocation, big_sur: "5cc9476ccee4b8359d3ddffed4e2e2faef68e352eb0204a43947eb0e716769e3"
+  end
+
+  depends_on "go" => :build
+  depends_on MacfuseRequirement
+  depends_on :macos
+
+  def install
+    # Build the build_gcsfuse tool. Ensure that it doesn't pick up any
+    # libraries from the user's GOPATH; it should have no dependencies.
+    ENV.delete("GOPATH")
+    system "go", "build", "./tools/build_gcsfuse"
+
+    # Use that tool to build gcsfuse itself.
+    gcsfuse_version = build.head? ? Utils.git_short_head : version
+    system "./build_gcsfuse", buildpath, prefix, gcsfuse_version
+  end
+
+  test do
+    system "#{bin}/gcsfuse", "--help"
+    system "#{sbin}/mount_gcsfuse", "--help"
+  end
+end
