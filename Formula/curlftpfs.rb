0db1ca334f1e647ab57a5a0e902982f0a088fb56
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 12:05:48 +0800

    curlftpfs: add 0.9.2 bottle.

---
+++ Formula/curlftpfs.rb
@@ -0,0 +1,36 @@
+require_relative "../require/macfuse"
+
+class Curlftpfs < Formula
+  desc "Filesystem for accessing FTP hosts based on FUSE and libcurl"
+  homepage "https://curlftpfs.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/curlftpfs/curlftpfs/0.9.2/curlftpfs-0.9.2.tar.gz"
+  sha256 "4eb44739c7078ba0edde177bdd266c4cfb7c621075f47f64c85a06b12b3c6958"
+  head ":pserver:anonymous:@curlftpfs.cvs.sourceforge.net:/cvsroot/curlftpfs", using: :cvs
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/curlftpfs-0.9.2"
+    sha256 cellar: :any, big_sur: "8d2060b8190ac5bb9ced1aebb2ff8c978a2a48e87ab2e1442f1dd6c4c5cc41c9"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on MacfuseRequirement
+  depends_on :macos
+
+  # TODO: depend on specific X11 formulae instead
+
+  def install
+    ENV.append "CPPFLAGS", "-D__off_t=off_t"
+    system "autoreconf", "-fvi"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/curlftpfs --version 2>&1", 1)
+  end
+end
