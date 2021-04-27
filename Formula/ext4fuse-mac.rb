00e1c0b2b39615e22084b2f82dacfa4b9f175ab4
Adrian Ho <the.gromgit@gmail.com>
2021-04-27 18:30:29 +0800

    ext4fuse-mac: update 0.1.3 bottle.

--- Formula/ext4fuse-mac.rb
+++ Formula/ext4fuse-mac.rb
@@ -10,9 +10,7 @@ class Ext4fuseMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/ext4fuse-mac-0.1.3"
-    sha256 cellar: :any, big_sur:  "e29a46d11968d75fd09e06ff1d0c1be7bf63f11e56ed3c4eacbfb223f0ad5d5d"
-    sha256 cellar: :any, catalina: "37c3b8fe51092e5b7053b1f12313b57baf7ba6afbc0c5069cad41bc834127dd9"
-    sha256 cellar: :any, mojave:   "3c4c0f771fb91a2c229dfd2f475e6fe1cc575a3c511893fc267d5ae8bb1df9a7"
+    sha256 cellar: :any, big_sur: "9105318c87415a8c9466580b92c4e370abffaab1addb33712fd9d6ff78ed4824"
   end
 
   depends_on "pkg-config" => :build
