1cf62e7361a6d952604db7b150cf25a20dee9356
Dave Nicolson <david.nicolson@gmail.com>
2021-05-01 17:51:39 +0200

    Add ifuse

---
+++ Formula/ifuse-mac.rb
@@ -0,0 +1,32 @@
+require_relative "../require/macfuse"
+
+class IfuseMac < Formula
+  desc "FUSE module for iOS devices"
+  homepage "https://www.libimobiledevice.org/"
+  url "https://github.com/libimobiledevice/ifuse/archive/1.1.4.tar.gz"
+  sha256 "2a00769e8f1d8bad50898b9d00baf12c8ae1cda2d19ff49eaa9bf580e5dbe78c"
+  license "LGPL-2.1"
+  head "https://cgit.sukimashita.com/ifuse.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libimobiledevice"
+  depends_on "libplist"
+  depends_on MacfuseRequirement
+  depends_on :macos
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # Actual test of functionality requires osxfuse, so test for expected failure instead
+    assert_match "ERROR: No device found!", shell_output("#{bin}/ifuse --list-apps", 1)
+  end
+end
