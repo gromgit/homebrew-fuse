b5eb3fe0392852dd87d480676b800d1c0f69d165
Adrian Ho <the.gromgit@gmail.com>
2021-05-04 16:59:30 +0800

    tup-mac: update 0.7.10 bottle.

---
+++ Formula/tup-mac.rb
@@ -0,0 +1,32 @@
+require_relative "../require/macfuse"
+
+class TupMac < Formula
+  desc "File-based build system"
+  homepage "http://gittup.org/tup/"
+  url "https://github.com/gittup/tup/archive/v0.7.10.tar.gz"
+  sha256 "c80946bc772ae4a5170855e907c866dae5040620e81ee1a590223bdbdf65f0f8"
+  license "GPL-2.0-only"
+  head "https://github.com/gittup/tup.git"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/tup-mac-0.7.10"
+    sha256 cellar: :any, big_sur: "b53eb5dc159f063f9fc2e744c866b7582acd3baafa9a8bee7f0df7cb22ab91c8"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on MacfuseRequirement
+  depends_on :macos
+
+  def install
+    ENV["TUP_LABEL"] = version
+    system "./build.sh"
+    bin.install "build/tup"
+    man1.install "tup.1"
+    doc.install (buildpath/"docs").children
+    pkgshare.install "contrib/syntax"
+  end
+
+  test do
+    system "#{bin}/tup", "-v"
+  end
+end
