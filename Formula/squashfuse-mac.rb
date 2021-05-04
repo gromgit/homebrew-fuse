1feb2b7d8cf164c2551971d9de1c2fd240d3a748
Adrian Ho <the.gromgit@gmail.com>
2021-05-04 16:46:02 +0800

    squashfuse-mac: update 0.1.104 bottle.

---
+++ Formula/squashfuse-mac.rb
@@ -0,0 +1,37 @@
+require_relative "../require/macfuse"
+
+class SquashfuseMac < Formula
+  desc "FUSE filesystem to mount squashfs archives"
+  homepage "https://github.com/vasi/squashfuse"
+  url "https://github.com/vasi/squashfuse/releases/download/0.1.104/squashfuse-0.1.104.tar.gz"
+  sha256 "aa52460559e0d0b1753f6b1af5c68cfb777ca5a13913285e93f4f9b7aa894b3a"
+  license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/squashfuse-mac-0.1.104"
+    sha256 cellar: :any, big_sur: "71e48214e5e13234e5270af7a8e4332adbf0a4e5b7b45fa754f6c06776b4b132"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "lz4"
+  depends_on "lzo"
+  depends_on "squashfs"
+  depends_on "xz"
+  depends_on "zstd"
+  depends_on MacfuseRequirement
+  depends_on :macos
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  # Unfortunately, making/testing a squash mount requires sudo privileges, so
+  # just test that squashfuse execs for now.
+  test do
+    output = shell_output("#{bin}/squashfuse --version 2>&1", 254)
+    assert_match version.to_s, output
+  end
+end
