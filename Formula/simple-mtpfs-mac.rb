490ef49721c51813419d8b664bf7343747e22f18
Adrian Ho <the.gromgit@gmail.com>
2021-05-04 16:26:59 +0800

    simple-mtpfs-mac: update 0.4.0 bottle.

--- Formula/simple-mtpfs-mac.rb
+++ Formula/simple-mtpfs-mac.rb
@@ -11,6 +11,7 @@ class SimpleMtpfsMac < Formula
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/simple-mtpfs-mac-0.4.0"
     sha256 cellar: :any, big_sur:  "ab2daeee7f6e6c6bd9795f34b7d1746b3e9add149e4a33e88db29be02de20541"
     sha256 cellar: :any, catalina: "6a07721c8bbd594f8ceeaf3a2503e780d3eb6ce3c4431ca785c40c8c71e8900c"
+    sha256 cellar: :any, mojave:   "bbe032821fb0ea78e661a16fd79a185d969e8b2a602fd990c8ceb035cc7fee84"
   end
 
   depends_on "autoconf" => :build
