c6661f7a487c40a621356574f260d349ef1d979a
Adrian Ho <the.gromgit@gmail.com>
2021-05-05 22:10:39 +0800

    xmount-mac: update 0.7.6 bottle.

---
+++ Formula/xmount-mac.rb
@@ -0,0 +1,45 @@
+require_relative "../require/macfuse"
+
+class XmountMac < Formula
+  desc "Convert between multiple input & output disk image types"
+  homepage "https://www.pinguin.lu/xmount/"
+  url "https://files.pinguin.lu/xmount-0.7.6.tar.gz"
+  sha256 "76e544cd55edc2dae32c42a38a04e11336f4985e1d59cec9dd41e9f9af9b0008"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/xmount-mac-0.7.6"
+    sha256 big_sur: "7759a60875ac63e16cf33d1b87376be2f1cc57adc2ac4653e71d7bc10b5707db"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "afflib"
+  depends_on "libewf"
+  depends_on MacfuseRequirement
+  depends_on :macos
+  depends_on "openssl@1.1"
+
+  patch :DATA
+
+  def install
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@1.1"].opt_lib/"pkgconfig"
+
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system bin/"xmount", "--version"
+  end
+end
+__END__
+--- xmount-0.7.6/cmake_modules/FindLibOSXFUSE.cmake.orig	2021-05-05 14:32:44.220213677 +0800
++++ xmount-0.7.6/cmake_modules/FindLibOSXFUSE.cmake	2021-05-05 14:35:07.185349574 +0800
+@@ -1,6 +1,6 @@
+ # Try pkg-config first
+ find_package(PkgConfig)
+-pkg_check_modules(PKGC_LIBOSXFUSE QUIET osxfuse)
++pkg_check_modules(PKGC_LIBOSXFUSE QUIET fuse)
+ 
+ if(PKGC_LIBOSXFUSE_FOUND)
+   # Found lib using pkg-config.
