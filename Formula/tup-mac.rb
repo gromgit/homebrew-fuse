82357a0630fa9bb15e7bed7da6def077ba41d75a
Adrian Ho <the.gromgit@gmail.com>
2021-06-02 08:24:18 +0800

    tup-mac: update 0.7.11 bottle.

--- Formula/tup-mac.rb
+++ Formula/tup-mac.rb
@@ -3,16 +3,14 @@ require_relative "../require/macfuse"
 class TupMac < Formula
   desc "File-based build system"
   homepage "http://gittup.org/tup/"
-  url "https://github.com/gittup/tup/archive/v0.7.10.tar.gz"
-  sha256 "c80946bc772ae4a5170855e907c866dae5040620e81ee1a590223bdbdf65f0f8"
+  url "https://github.com/gittup/tup/archive/v0.7.11.tar.gz"
+  sha256 "be24dff5f1f32cc85c73398487a756b4a393adab5e4d8500fd5164909d3e85b9"
   license "GPL-2.0-only"
   head "https://github.com/gittup/tup.git"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/tup-mac-0.7.10"
-    sha256 cellar: :any, big_sur:  "b53eb5dc159f063f9fc2e744c866b7582acd3baafa9a8bee7f0df7cb22ab91c8"
-    sha256 cellar: :any, catalina: "d9d25e760b8b9b29cb73bdb1e573a383a3a5a8698e9437f272b66108103bc7de"
-    sha256 cellar: :any, mojave:   "86f1ea5505469a269a5aabba0f4138451652e1fb9b36544b9e9a818770bf2f56"
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/tup-mac-0.7.11"
+    sha256 cellar: :any, big_sur: "e1d223dcd5865119de5a2b9570ef28ec780909d709db49f53c3fe7229aa68e5a"
   end
 
   depends_on "pkg-config" => :build
