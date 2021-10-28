d8f5233494263fe70e906dab1f3fe1bc79199a2f
Adrian Ho <the.gromgit@gmail.com>
2021-10-28 19:48:00 +0800

    securefs-mac: update 0.12.0 bottle.

--- Formula/securefs-mac.rb
+++ Formula/securefs-mac.rb
@@ -4,23 +4,19 @@ class SecurefsMac < Formula
   desc "Filesystem with transparent authenticated encryption"
   homepage "https://github.com/netheril96/securefs"
   url "https://github.com/netheril96/securefs.git",
-      tag:      "0.11.1",
-      revision: "dfeebf8406871d020848edde668234715356158c"
+      tag:      "0.12.0",
+      revision: "a4972834d93e89117e67dae58998f10f8a7c0fbb"
   license "MIT"
   head "https://github.com/netheril96/securefs.git"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/securefs-mac-0.11.1"
-    sha256 cellar: :any, big_sur:  "67f46a42eca522dbc42cc5c8b2ac75b1f367db0810494cf0e6a196723ce32270"
-    sha256 cellar: :any, catalina: "ca8ed21a783afb192376d09c0e60a49da6ff20af048c7f121306fc5078acb28b"
-    sha256 cellar: :any, mojave:   "2b37ead322ab277faf07d4c6357d426aa58a5186af5ec9246792e8e8392e0da5"
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/securefs-mac-0.12.0"
+    sha256 cellar: :any, big_sur: "70db42eeea99f3ba3f2cc630e54d889160557a578c288fab05c3a4a16f6313b1"
   end
 
   depends_on "cmake" => :build
   depends_on MacfuseRequirement
   depends_on :macos
-
-  patch :DATA
 
   def install
     setup_fuse
@@ -32,21 +28,3 @@ class SecurefsMac < Formula
     system "#{bin}/securefs", "version" # The sandbox prevents a more thorough test
   end
 end
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 7b5e57d..7c9d2b7 100755
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -19,11 +19,7 @@ configure_file(${CMAKE_SOURCE_DIR}/sources/git-version.cpp.in ${CMAKE_BINARY_DIR
- 
- if (UNIX)
-     find_path(FUSE_INCLUDE_DIR fuse.h PATHS /usr/local/include PATH_SUFFIXES osxfuse)
--    if (APPLE)
--        find_library(FUSE_LIBRARIES osxfuse PATHS /usr/local/lib)
--    else()
--        find_library(FUSE_LIBRARIES fuse PATHS /usr/local/lib)
--    endif()
-+    find_library(FUSE_LIBRARIES fuse PATHS /usr/local/lib)
-     include_directories(${FUSE_INCLUDE_DIR})
-     link_libraries(${FUSE_LIBRARIES})
-     add_compile_options(-Wall -Wextra -Wno-unknown-pragmas)
