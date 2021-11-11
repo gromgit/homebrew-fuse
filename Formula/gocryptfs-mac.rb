c7a8707aab2a864cb305d86a61c01d00c41781e7
Adrian Ho <the.gromgit@gmail.com>
2021-11-12 00:51:45 +0800

    gocryptfs-mac: update 2.2.1 bottle.

--- Formula/gocryptfs-mac.rb
+++ Formula/gocryptfs-mac.rb
@@ -9,6 +9,7 @@ class GocryptfsMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/gocryptfs-mac-2.2.1"
+    sha256 cellar: :any, monterey: "e0c267b091391fc0d7af3d1411dc22d68a8325427b884d9a01273918704c09d2"
     sha256 cellar: :any, big_sur:  "1c618b3a27524e560372f5d26d0718434f04a32ffe6cd0faa18b39f6019df13d"
     sha256 cellar: :any, catalina: "928575ec0f8aeeeae29a15bbc71d611338a17b672e42b2fdb512c7ee55ecf091"
     sha256 cellar: :any, mojave:   "22a05bc7c7d6c0d5b36261e3ff9f02d8e475b24efcaa82d4d650aa02348a9b0d"
