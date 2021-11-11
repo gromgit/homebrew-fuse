813a2d44dba3141d3c800c837302dd1be4f7c0df
Adrian Ho <the.gromgit@gmail.com>
2021-11-12 00:25:48 +0800

    avfs-mac: update 1.1.4 bottle.

--- Formula/avfs-mac.rb
+++ Formula/avfs-mac.rb
@@ -8,6 +8,7 @@ class AvfsMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/avfs-mac-1.1.4"
+    sha256 monterey: "73ab1cbd3df13518d52387613e24f0550e5a39c4d49f7ab58b20e4006ca5058f"
     sha256 big_sur:  "536a8ff3129d4ca73bafa08d059ecaa057dec2a24b0c4509762e8f62ad1117ca"
     sha256 catalina: "edfe514eaacc649484b26a67f37a2c8aa38d4bf7cff97ff06477417df4396701"
     sha256 mojave:   "eb4171d8c40b058d72fb0cbf480c21c0a89b201498fcc37d2dfd4c050219aacd"
