bb5afc331116a7c799383b5e370b1b845cfe264c
Adrian Ho <the.gromgit@gmail.com>
2022-01-01 00:59:16 +0800

    rclone-mac: update 1.57.0 bottle.

--- Formula/rclone-mac.rb
+++ Formula/rclone-mac.rb
@@ -12,6 +12,7 @@ class RcloneMac < Formula
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/rclone-mac-1.57.0"
     sha256 cellar: :any_skip_relocation, monterey: "6781de4b7a88e170fc653110160b2eb633bd780b7dff68bcb8ee8db5508ee259"
     sha256 cellar: :any_skip_relocation, big_sur:  "a5f85ac8d5c2dc818e092e5f7f75284ece39f3386717df0842ffda1be6d47df3"
+    sha256 cellar: :any_skip_relocation, catalina: "f37cf21d56b5118406a3625a5f32f9c26d3b43eb23a63a7847ca195d2cc88d33"
   end
 
   depends_on "go" => :build
