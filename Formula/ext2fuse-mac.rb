20b493d84939e09b0c29ddd38132ef58a81f3316
Adrian Ho <the.gromgit@gmail.com>
2022-01-08 22:36:18 +0800

    ext2fuse-mac: update 0.8.1 bottle.

--- Formula/ext2fuse-mac.rb
+++ Formula/ext2fuse-mac.rb
@@ -8,10 +8,11 @@ class Ext2fuseMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/ext2fuse-mac-0.8.1"
-    sha256 cellar: :any, monterey: "73a7f099cb28d5fd7a25059da15a6cd7827fa991108c68b8de588b38aecb2bc0"
-    sha256 cellar: :any, big_sur:  "8dba44571e8df43bac8356f7fe74719200f965318e9075fdc717e863dbf8aad7"
-    sha256 cellar: :any, catalina: "0b24132b10c92b0f9ca6bde9c72caa54281e812c07f1f491034fe71e305eba67"
-    sha256 cellar: :any, mojave:   "e46c5e7eff79716a8e075752e429bea3e9027b6516538df45fbbdc98ba878b94"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "db2bd76b01aea52dd68f9659126fdc87adeb15ad561fe60af20ff09ce9327e51"
+    sha256 cellar: :any,                 monterey:       "73a7f099cb28d5fd7a25059da15a6cd7827fa991108c68b8de588b38aecb2bc0"
+    sha256 cellar: :any,                 big_sur:        "8dba44571e8df43bac8356f7fe74719200f965318e9075fdc717e863dbf8aad7"
+    sha256 cellar: :any,                 catalina:       "0b24132b10c92b0f9ca6bde9c72caa54281e812c07f1f491034fe71e305eba67"
+    sha256 cellar: :any,                 mojave:         "e46c5e7eff79716a8e075752e429bea3e9027b6516538df45fbbdc98ba878b94"
   end
 
   depends_on "gcc" => :build
