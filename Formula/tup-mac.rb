1b3b7fdbb39fe51b049df7fd5430cbde2ffe6ae0
Adrian Ho <the.gromgit@gmail.com>
2022-01-08 23:22:56 +0800

    tup-mac: update 0.7.11 bottle.

--- Formula/tup-mac.rb
+++ Formula/tup-mac.rb
@@ -10,10 +10,11 @@ class TupMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/tup-mac-0.7.11"
-    sha256 cellar: :any, monterey: "c0fa166568e8f24a6a77de27ec30f201d65ae812b1b595fd0cbea44647aa69e4"
-    sha256 cellar: :any, big_sur:  "e1d223dcd5865119de5a2b9570ef28ec780909d709db49f53c3fe7229aa68e5a"
-    sha256 cellar: :any, catalina: "ce44cb9e6ca8c79e35a2cc4e2dfd7c39460bb2575510ac3ee966b6b7d6642679"
-    sha256 cellar: :any, mojave:   "03d842d15270c158614812b9355bb91252994f4b2b7b0a2a308b336b312784f8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "080770170cafca605d4f04b26e32ea8af093df86a6461fee483073f404de62aa"
+    sha256 cellar: :any,                 monterey:       "c0fa166568e8f24a6a77de27ec30f201d65ae812b1b595fd0cbea44647aa69e4"
+    sha256 cellar: :any,                 big_sur:        "e1d223dcd5865119de5a2b9570ef28ec780909d709db49f53c3fe7229aa68e5a"
+    sha256 cellar: :any,                 catalina:       "ce44cb9e6ca8c79e35a2cc4e2dfd7c39460bb2575510ac3ee966b6b7d6642679"
+    sha256 cellar: :any,                 mojave:         "03d842d15270c158614812b9355bb91252994f4b2b7b0a2a308b336b312784f8"
   end
 
   depends_on "pkg-config" => :build
