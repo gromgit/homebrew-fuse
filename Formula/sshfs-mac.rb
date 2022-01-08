1e607e0673c2d472dd870aeef64b83ddfe1dc676
Adrian Ho <the.gromgit@gmail.com>
2022-01-08 23:22:21 +0800

    sshfs-mac: update 2.10_1 bottle.

--- Formula/sshfs-mac.rb
+++ Formula/sshfs-mac.rb
@@ -10,10 +10,11 @@ class SshfsMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/sshfs-mac-2.10"
-    sha256 cellar: :any, monterey: "ffbc11b371d196dd6a32c13b21dfc28af7f7577145a62a165062228c2c661263"
-    sha256 cellar: :any, big_sur:  "9e021d24580ec55f4ab034dba0e264906d9db5c57036fe83710bc601bc6885c6"
-    sha256 cellar: :any, catalina: "09f254420411218a784c783df760d2c652acd280eecc875d60e41014f80011cc"
-    sha256 cellar: :any, mojave:   "6389b69b921295f5be6eb35336649f558375eb24da60573b376a313331a4d18c"
+    sha256 cellar: :any, arm64_monterey: "422abbbbd1804e67c27a21b976e4e82cfdffd977b5aa2cf7ee4e21fc2b548167"
+    sha256 cellar: :any, monterey:       "ffbc11b371d196dd6a32c13b21dfc28af7f7577145a62a165062228c2c661263"
+    sha256 cellar: :any, big_sur:        "9e021d24580ec55f4ab034dba0e264906d9db5c57036fe83710bc601bc6885c6"
+    sha256 cellar: :any, catalina:       "09f254420411218a784c783df760d2c652acd280eecc875d60e41014f80011cc"
+    sha256 cellar: :any, mojave:         "6389b69b921295f5be6eb35336649f558375eb24da60573b376a313331a4d18c"
   end
 
   depends_on "autoconf" => :build
