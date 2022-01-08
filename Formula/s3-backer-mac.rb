f340773127fbc21be0786db4939843414415d3ff
Adrian Ho <the.gromgit@gmail.com>
2022-01-08 23:16:51 +0800

    s3-backer-mac: update 1.6.3 bottle.

--- Formula/s3-backer-mac.rb
+++ Formula/s3-backer-mac.rb
@@ -9,10 +9,11 @@ class S3BackerMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/s3-backer-mac-1.6.3"
-    sha256 cellar: :any, monterey: "fbba88dee405e54a779e2d44d00cf35c6fd07fcba8df09edd9c8dc354113ac46"
-    sha256 cellar: :any, big_sur:  "08a15a5a5096c36e415caf1e3de2c79566b7c515558f844801f0dd6055cbe3aa"
-    sha256 cellar: :any, catalina: "693c50b9164d039d82af8417cbdf40da26cb6a8e847b4a8de90572ad56cc3c2d"
-    sha256 cellar: :any, mojave:   "6d73421febd44083336ff46ac5d1c9f16cb4a1709097ae61fbf8b4e7c29be114"
+    sha256 cellar: :any, arm64_monterey: "b61d30670b020cb27c8acc9ce7888774dc1ce4f2aa42df4cc96cbc3ec98be389"
+    sha256 cellar: :any, monterey:       "fbba88dee405e54a779e2d44d00cf35c6fd07fcba8df09edd9c8dc354113ac46"
+    sha256 cellar: :any, big_sur:        "08a15a5a5096c36e415caf1e3de2c79566b7c515558f844801f0dd6055cbe3aa"
+    sha256 cellar: :any, catalina:       "693c50b9164d039d82af8417cbdf40da26cb6a8e847b4a8de90572ad56cc3c2d"
+    sha256 cellar: :any, mojave:         "6d73421febd44083336ff46ac5d1c9f16cb4a1709097ae61fbf8b4e7c29be114"
   end
 
   depends_on "pkg-config" => :build
