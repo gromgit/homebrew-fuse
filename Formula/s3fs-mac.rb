b9f33acb9e053cea653335bed01da9d6f99c6e5f
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 14:54:14 +0800

    s3fs-mac: update 1.91 bottle.

--- Formula/s3fs-mac.rb
+++ Formula/s3fs-mac.rb
@@ -12,6 +12,7 @@ class S3fsMac < Formula
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/s3fs-mac-1.91"
     sha256 cellar: :any, arm64_monterey: "a433fd964e67240ff3135f19aa4de7e1dcdbe331a339ba52f7803241032810ea"
     sha256 cellar: :any, big_sur:        "b9e8bb0525cd245c65cc7008d81898f918e12589dcd6cebf1dbe5f0157988ed2"
+    sha256 cellar: :any, catalina:       "694c4f8d8a2fc059226df89992438ee12a2dc420775ff52e48e9a85c7c868612"
     sha256 cellar: :any, mojave:         "6888b53265c1e3d0432d308acdc3b86779a949d89a77162555bafed73641508a"
   end
 
