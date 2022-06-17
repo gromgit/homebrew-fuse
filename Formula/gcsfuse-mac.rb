d73d7a0ce7fc2445783d0b6a03b5b32f72caa3d7
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 11:09:28 +0800

    gcsfuse-mac: update 0.41.2 bottle.

--- Formula/gcsfuse-mac.rb
+++ Formula/gcsfuse-mac.rb
@@ -3,22 +3,25 @@ require_relative "../require/macfuse"
 class GcsfuseMac < Formula
   desc "User-space file system for interacting with Google Cloud"
   homepage "https://github.com/googlecloudplatform/gcsfuse"
-  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.35.1.tar.gz"
-  sha256 "effcbffa238cf0a97488b4a3b836c0996b1db17a18ad91bf76b5c195a4f5bfed"
+  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.41.2.tar.gz"
+  sha256 "6a64c30c28651978cf51685051ea9af1d45b9f3d5b3ded894af82c17883a34c4"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/gcsfuse.git"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/gcsfuse-mac-0.35.1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "42983de1de81aba6500d6e84f926eeffa97ec1081f39bfe5f06a565d427fe65c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5c49e2ba988a1fda0ca3f28ca2745c37f1e6b8abd2eff55b91b857b1907fe068"
-    sha256 cellar: :any_skip_relocation, catalina:       "9824cf0f7e905dc207a5591a233f12a6c9130bfafa6e04c2447506b447a6c52f"
-    sha256 cellar: :any_skip_relocation, mojave:         "6322a98c0157d6d5cf7ea56f564dfb90f1bd6bc51f0786d24fb8aca3460fbe22"
+    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/gcsfuse-mac-0.41.2"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a4ed90a795bb34afdf7a77aa38c6322be0d94c501a60daa0aecce138ecbdfd5b"
   end
 
   depends_on "go" => :build
   depends_on MacfuseRequirement
   depends_on :macos
+
+  # Review for removal on next release
+  patch do
+    url "https://github.com/GoogleCloudPlatform/gcsfuse/commit/c2abca911ff03b84ab64214b6717d8d7cc74c10f.patch?full_index=1"
+    sha256 "62930a0ae8322a071d489b1dd386206742b962123312b1368589c731867945b4"
+  end
 
   def install
     setup_fuse
@@ -29,7 +32,7 @@ class GcsfuseMac < Formula
 
     # Use that tool to build gcsfuse itself.
     gcsfuse_version = build.head? ? Utils.git_short_head : version
-    system "./build_gcsfuse", buildpath, prefix, gcsfuse_version
+    system "./build_gcsfuse", buildpath, prefix, gcsfuse_version, "-buildvcs=false"
   end
 
   test do
