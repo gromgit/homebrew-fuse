4d2c3d6234b042d96dadabccc0dd791defcb9416
Adrian Ho <aho+vm-bigsur@03s.net>
2022-09-05 11:07:15 +0800

    bindfs-mac: update 1.17.0 bottle.

--- Formula/bindfs-mac.rb
+++ Formula/bindfs-mac.rb
@@ -9,6 +9,7 @@ class BindfsMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/bindfs-mac-1.17.0"
+    sha256 cellar: :any, big_sur:  "438df92ca7ec74d8d30897ad4bc9f45ba10a8a97523df5892ac8970992819bcf"
     sha256 cellar: :any, catalina: "ccb850e290a88fa78930c2920854766385d3cfeb8e4537278cc918745a88a056"
     sha256 cellar: :any, mojave:   "c48f0834685c79d272f7af5801f9b8cab86686b5d0e82b255b028f1f4a57a115"
   end
