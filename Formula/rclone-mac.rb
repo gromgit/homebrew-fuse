5019f285809e653ffdbde0de64c7cc3c655fb45f
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 22:28:36 +0800

    rclone-mac: update 1.59.1 bottle.

--- Formula/rclone-mac.rb
+++ Formula/rclone-mac.rb
@@ -13,6 +13,7 @@ class RcloneMac < Formula
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e53d325873d6ad26303fd23ba4c14d793b70c703df056a0e4ba2226edb9041ca"
     sha256 cellar: :any_skip_relocation, monterey:       "4362bcdf29a86e5398227c90470215850fc77c375b8355241d6a4ed3bb7fd6db"
     sha256 cellar: :any_skip_relocation, big_sur:        "6a61d21b1ccaaa35aba43ad4b56b51be8cec31d17554d3031a091f662a56bcef"
+    sha256 cellar: :any_skip_relocation, catalina:       "b281de99b5f2f17cd22d9a87dfd8f2bb15c0be8231c6afef173444f4434a2f93"
   end
 
   depends_on "go" => :build
