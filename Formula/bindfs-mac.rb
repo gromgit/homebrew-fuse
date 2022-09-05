6e9fee1d0d64b0419a42bc501028f55d1e79113c
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 11:10:30 +0800

    bindfs-mac: update 1.17.0 bottle.

--- Formula/bindfs-mac.rb
+++ Formula/bindfs-mac.rb
@@ -9,10 +9,11 @@ class BindfsMac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/bindfs-mac-1.17.0"
-    sha256 cellar: :any, monterey: "a89ee7436ed6268759ab8d118ae33f2e7ef08f9f6f4362c6b9d86c1b80660bf3"
-    sha256 cellar: :any, big_sur:  "438df92ca7ec74d8d30897ad4bc9f45ba10a8a97523df5892ac8970992819bcf"
-    sha256 cellar: :any, catalina: "ccb850e290a88fa78930c2920854766385d3cfeb8e4537278cc918745a88a056"
-    sha256 cellar: :any, mojave:   "c48f0834685c79d272f7af5801f9b8cab86686b5d0e82b255b028f1f4a57a115"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c55b77b90b0de6383e757f854da33b7fdb188d17128dcf0848342ba0aba67162"
+    sha256 cellar: :any,                 monterey:       "a89ee7436ed6268759ab8d118ae33f2e7ef08f9f6f4362c6b9d86c1b80660bf3"
+    sha256 cellar: :any,                 big_sur:        "438df92ca7ec74d8d30897ad4bc9f45ba10a8a97523df5892ac8970992819bcf"
+    sha256 cellar: :any,                 catalina:       "ccb850e290a88fa78930c2920854766385d3cfeb8e4537278cc918745a88a056"
+    sha256 cellar: :any,                 mojave:         "c48f0834685c79d272f7af5801f9b8cab86686b5d0e82b255b028f1f4a57a115"
   end
 
   head do
