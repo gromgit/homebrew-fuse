3d57e6de21637a75a891d5e94cbfb0b34c28dc9a
Adrian Ho <aho+bruteforce@03s.net>
2023-03-14 22:01:48 +0800

    mounty: transferred from homebrew/cask

---
+++ Casks/mounty.rb
@@ -0,0 +1,31 @@
+cask "mounty" do
+  on_catalina :or_older do
+    version "1.9"
+    sha256 "5fcedfe712f59c14f39c3385dfed9aebc99d4e8d88f6e870f364cc48624590ef"
+
+    livecheck do
+      skip "newer versions only available for Big Sur or higher"
+    end
+  end
+  on_big_sur :or_newer do
+    version "2.0"
+    sha256 "52bfb90e54368c62ad6fb980af610c6b30094426bbd22853265a8788e7e4e8eb"
+
+    livecheck do
+      url :homepage
+      regex(/Latest\s+version:\s*(\d+(?:\.\d+)+)/i)
+    end
+  end
+
+  url "https://mounty.app/releases/Mounty-#{version}.dmg"
+  name "Mounty for NTFS"
+  desc "Re-mounts write-protected NTFS volumes"
+  homepage "https://mounty.app/"
+
+  depends_on cask: "macfuse"
+  depends_on formula: "gromgit/fuse/ntfs-3g-mac"
+
+  app "Mounty.app"
+
+  zap trash: "~/Library/Preferences/com.cu4uc.mounty.plist"
+end
