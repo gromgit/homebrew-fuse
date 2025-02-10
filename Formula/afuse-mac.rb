be87b72d9010a8269383038373a8d21f386d97ef
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-02-10 16:03:28 +0000

    afuse-mac: update 0.5.0 bottle.

--- Formula/afuse-mac.rb
+++ Formula/afuse-mac.rb
@@ -9,12 +9,9 @@ class AfuseMac < Formula
   license "GPL-2.0-only"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/afuse-mac-0.4.1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "01b0427125830477ef5e6780d4a68d2bae93c78f7b60e7973e7bb60e7b177057"
-    sha256 cellar: :any,                 monterey:       "5641548745baf07dfae84ae670c96f202ae84b94e12c2fed60eed4aecf3bafc4"
-    sha256 cellar: :any,                 big_sur:        "d14a7e51b8d3fd8c7b3419ab06011e5e14c89b6d8c8f46dbe66b80ea3c97fa1f"
-    sha256 cellar: :any,                 catalina:       "bf5f4add8d2e8a2c9ad50e2508771f3c51fded35c21f7a23cf95b364e98f9c7a"
-    sha256 cellar: :any,                 mojave:         "577023bd06623a90ca245be88fbb49041a71cc1e4852195dc5d9d3b2bbdaf617"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "d0b0e74b43c27ce951eec136e4c45a98c43edc39cceeb95418f43c2d5bfa9afe"
+    sha256 cellar: :any,                 ventura:      "81eac9045ff5882fa86b3e40c6562bbed06cf1346ec5fb368033511cab03c982"
   end
 
   depends_on "autoconf" => :build
