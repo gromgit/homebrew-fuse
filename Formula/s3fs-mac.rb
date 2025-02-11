6b61113a7bea551c20206dc14456958e9935aa47
Adrian Ho <the.gromgit@gmail.com>
2025-02-11 23:14:22 +0800

    s3fs-mac 1.95

--- Formula/s3fs-mac.rb
+++ Formula/s3fs-mac.rb
@@ -3,10 +3,10 @@ require_relative "../require/macfuse"
 class S3fsMac < Formula
   desc "FUSE-based file system backed by Amazon S3"
   homepage "https://github.com/s3fs-fuse/s3fs-fuse/wiki"
-  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.92.tar.gz"
-  sha256 "76ebea3c0784c5c0f6b84e009d555806aff86258886ced39eee316bf02ae8750"
+  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.95.tar.gz"
+  sha256 "0c97b8922f005500d36f72aee29a1345c94191f61d795e2a7b79fb7e3e6f5517"
   license "GPL-2.0-or-later"
-  head "https://github.com/s3fs-fuse/s3fs-fuse.git"
+  head "https://github.com/s3fs-fuse/s3fs-fuse.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/s3fs-mac-1.92"
@@ -17,9 +17,11 @@ class S3fsMac < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkgconf" => :build
+  depends_on "curl"
   depends_on "gnutls"
   depends_on "libgcrypt"
+  depends_on "libxml2"
   depends_on MacfuseRequirement
   depends_on :macos
   depends_on "nettle"
@@ -27,7 +29,7 @@ class S3fsMac < Formula
   def install
     setup_fuse
     system "./autogen.sh"
-    system "./configure", "--disable-dependency-tracking", "--with-gnutls", "--prefix=#{prefix}"
+    system "./configure", "--with-gnutls", *std_configure_args
     system "make", "install"
   end
 
