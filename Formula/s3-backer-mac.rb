eafb545b81a0d97e31ed29d6c0c7095d5f44ebfb
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-02-11 15:20:19 +0000

    s3-backer-mac: update 2.1.3 bottle.

--- Formula/s3-backer-mac.rb
+++ Formula/s3-backer-mac.rb
@@ -8,12 +8,9 @@ class S3BackerMac < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/s3-backer-mac-2.0.2"
-    sha256 cellar: :any, arm64_monterey: "2f1d3837847ac2879eaa859c40f223c2956759a72768f8d03db1df03ec72cbf4"
-    sha256 cellar: :any, monterey:       "8a9f76908ee913f79aa7e32b5913f63d556510ac75cf254b9b86653dfcac05aa"
-    sha256 cellar: :any, big_sur:        "1aac74d23e01950841770936f418fba4cec9f64f97ee27120908af7db9e1c185"
-    sha256 cellar: :any, catalina:       "6b043afad65435edf6209a85d5e6c4c7d15752126af4656144e17d3fd7e994d8"
-    sha256 cellar: :any, mojave:         "a6d732072a6b2992c3cb08adcedac1fff5472da7e7c1858c57508b84af5570c2"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    sha256 cellar: :any, arm64_sonoma: "74c3dc5284de2816f8ac93749c530cb6031a88cb862cd6b3cd815fc962d61c7f"
+    sha256 cellar: :any, ventura:      "9e4cc5154864642ffaaea319c13994eab5ae558ca0278ad67f1ffc3aeaa7d95a"
   end
 
   depends_on "pkgconf" => :build
