28d66921cc35e79e8f1b951731fc83bf423e9f10
Adrian Ho <the.gromgit@gmail.com>
2025-02-12 18:19:22 +0800

    encfs: upgrade openssl + other fixes
    Closes #87.
    
    Signed-off-by: Adrian Ho <215702+gromgit@users.noreply.github.com>

--- Formula/encfs-mac.rb
+++ Formula/encfs-mac.rb
@@ -7,8 +7,9 @@ class EncfsMac < Formula
   sha256 "4709f05395ccbad6c0a5b40a4619d60aafe3473b1a79bafb3aa700b1f756fd63"
   # The code comprising the EncFS library (libencfs) is licensed under the LGPL.
   # The main programs (encfs, encfsctl, etc) are licensed under the GPL.
-  license all_of: ["GPL-3.0-only", "LGPL-3.0-only", "MIT", "Zlib"]
-  head "https://github.com/vgough/encfs.git"
+  license "GPL-3.0-or-later"
+  revision 1
+  head "https://github.com/vgough/encfs.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-fuse/releases/download/encfs-mac-1.9.5"
@@ -20,20 +21,27 @@ class EncfsMac < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkgconf" => :build
   depends_on "gettext"
   depends_on MacfuseRequirement
   depends_on :macos
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
+  depends_on "tinyxml2"
+
+  patch do
+    url "https://github.com/vgough/encfs/commit/75080681626062e5832aec0b1bb3aa37d8364822.patch?full_index=1"
+    sha256 "0222bc4a4f03541b1523b03471f6af5925d4ed2a4c0d36a9a6fe39a18c036770"
+  end
 
   def install
     setup_fuse
     ENV.cxx11
-
-    mkdir "build" do
-      system "cmake", "..", *fuse_cmake_args, *std_cmake_args
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_UNIT_TESTS=OFF",
+                    "-DUSE_INTERNAL_TINYXML=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
