5537e40d823d47d511160ccfeb3b9a1a87627626
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-02-12 12:24:40 +0000

    btfs-mac: update 2.24_1 bottle.

--- Formula/btfs-mac.rb
+++ Formula/btfs-mac.rb
@@ -10,12 +10,9 @@ class BtfsMac < Formula
   head "https://github.com/johang/btfs.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/btfs-mac-2.24"
-    sha256 cellar: :any, arm64_monterey: "725bfa8256133728271bd4c586ae03d2ed1922575ecf3ff4305a80becaaf8a06"
-    sha256 cellar: :any, monterey:       "443e68c98b9a5861e90993b1afc2552804cb688082b399700ff6326d3f9fc08e"
-    sha256 cellar: :any, big_sur:        "d92d62980a61112eaf05240b150122226cabaaeb9e52910d06a3db5475c9cf0d"
-    sha256 cellar: :any, catalina:       "8820a96d3817703851cae7871711fc635a94dfc419fffeba70155f8fd09a21bf"
-    sha256 cellar: :any, mojave:         "10516fbacf59d585f1f28fc5c883f77724de7b1e49c094aabb0fe5ee4c8e07e5"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    sha256 cellar: :any, arm64_sonoma: "71e05be0beb418720d313a63f10cfea741d648bf78630b497eb2c51a5003d319"
+    sha256 cellar: :any, ventura:      "388cc358e3c9372768469b6b1021f43d8def07f372aaef6e3f5d2d80bc93be48"
   end
 
   depends_on "autoconf" => :build
