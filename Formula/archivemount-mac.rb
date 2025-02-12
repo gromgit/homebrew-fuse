d5391b7159788a6d6e4456c6497dfdfd06457e3a
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-02-12 12:35:41 +0000

    archivemount-mac: update 0.9.1 bottle.

--- Formula/archivemount-mac.rb
+++ Formula/archivemount-mac.rb
@@ -12,12 +12,10 @@ class ArchivemountMac < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/archivemount-mac-0.9.1"
-    sha256 cellar: :any, arm64_monterey: "c79ff7d0b53fbdad256912dbfd3e92727e0f68d5248281e1ffdac941b1b1cfc5"
-    sha256 cellar: :any, monterey:       "b938c74e2a690d66ebb85a82e75cbca0a9724d7fdb8596818588975a1f1adad5"
-    sha256 cellar: :any, big_sur:        "84dfb26c79c5d3cd6596bbbbb1398dd7d0a855eab32ff2b1192cd36235c34ab9"
-    sha256 cellar: :any, catalina:       "0d838c1b6684201cf9ff7bcc1120052bfce87c31950622961852a9e17243cd94"
-    sha256 cellar: :any, mojave:         "42170c737f1fd151a4e36276c6929fe49aa69137886238ec38f04a84c123f26b"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    rebuild 1
+    sha256 cellar: :any, arm64_sonoma: "abe39d7d99aae950033e62df6c1ac3c33b7b8a7863eeb92d2bb82c1b3b5662fc"
+    sha256 cellar: :any, ventura:      "be2622a6cb26b8dc581c35b1cad1f07ddafbfea379e5f4db250258ab6583f615"
   end
 
   depends_on "pkgconf" => :build
