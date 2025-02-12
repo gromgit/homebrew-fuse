d2bffbe2378905c5214d134c1b0a997b0f9f09e8
Adrian Ho <the.gromgit@gmail.com>
2025-02-12 20:32:00 +0800

    curlftpfs: update build

--- Formula/curlftpfs-mac.rb
+++ Formula/curlftpfs-mac.rb
@@ -2,9 +2,10 @@ require_relative "../require/macfuse"
 
 class CurlftpfsMac < Formula
   desc "Filesystem for accessing FTP hosts based on FUSE and libcurl"
-  homepage "https://curlftpfs.sourceforge.io/"
+  homepage "https://curlftpfs.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/curlftpfs/curlftpfs/0.9.2/curlftpfs-0.9.2.tar.gz"
   sha256 "4eb44739c7078ba0edde177bdd266c4cfb7c621075f47f64c85a06b12b3c6958"
+  license "GPL-2.0-only"
   head ":pserver:anonymous:@curlftpfs.cvs.sourceforge.net:/cvsroot/curlftpfs", using: :cvs
 
   bottle do
@@ -19,19 +20,17 @@ class CurlftpfsMac < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkgconf" => :build
+  depends_on "curl"
   depends_on "glib"
   depends_on MacfuseRequirement
   depends_on :macos
 
-  # TODO: depend on specific X11 formulae instead
-
   def install
     setup_fuse
     ENV.append "CPPFLAGS", "-D__off_t=off_t"
-    system "autoreconf", "-fvi"
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "autoreconf", "--force", "--install", "--verbose"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
