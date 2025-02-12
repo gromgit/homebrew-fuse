d1936a242f9f82530450ff618dc19e78010f0f74
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-02-12 15:45:41 +0000

    simple-mtpfs-mac: update 0.4.0 bottle.

--- Formula/simple-mtpfs-mac.rb
+++ Formula/simple-mtpfs-mac.rb
@@ -8,12 +8,10 @@ class SimpleMtpfsMac < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/simple-mtpfs-mac-0.4.0"
-    sha256 cellar: :any, arm64_monterey: "3d5479cfca4348b32a0dc38097e803e817865488efe44de1e91cbcc1e39bef0f"
-    sha256 cellar: :any, monterey:       "dab8ca53f1513d943c8102be2d592adf42d36f6d62d65fdd0af5914e7786c530"
-    sha256 cellar: :any, big_sur:        "ab2daeee7f6e6c6bd9795f34b7d1746b3e9add149e4a33e88db29be02de20541"
-    sha256 cellar: :any, catalina:       "6a07721c8bbd594f8ceeaf3a2503e780d3eb6ce3c4431ca785c40c8c71e8900c"
-    sha256 cellar: :any, mojave:         "bbe032821fb0ea78e661a16fd79a185d969e8b2a602fd990c8ceb035cc7fee84"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    rebuild 1
+    sha256 cellar: :any, arm64_sonoma: "cf831fa0da5d53fbef60c754223bb2ed7601c80e89d7bb11be4bd349001b2f1e"
+    sha256 cellar: :any, ventura:      "01adbaed0737e09f73ec91d8e248a1a9c138e06f8476cd6da23e6b511f3c68d9"
   end
 
   depends_on "autoconf" => :build
