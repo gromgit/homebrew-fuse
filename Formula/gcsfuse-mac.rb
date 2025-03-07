303f05c88df064248d0c62b931ab5e7273f101f5
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-03-07 09:11:25 +0000

    gcsfuse-mac: update 2.4.0 bottle.

--- Formula/gcsfuse-mac.rb
+++ Formula/gcsfuse-mac.rb
@@ -14,10 +14,9 @@ class GcsfuseMac < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/gcsfuse-mac-0.42.5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4c8fe0405b5bd69ad29b9a2d64034c332a190d5c6772fd51c83a40e431ad9ac2"
-    sha256 cellar: :any_skip_relocation, monterey:       "293e9a8e9e00e58df3ff3dd90c9bb81a53d3345c412c4d6cf8b1e344484e1283"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7aae298075c4ab1a56635d3bec54360e6986c59596b66a132fcc2cdba8633a14"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "676453470943e7c5ebf32f192a8e09d4aa7d11c4fc8411476b514bf60716e155"
+    sha256 cellar: :any_skip_relocation, ventura:      "feef9dc9b3983462e6c60082f410e9e8ec05274f1712435711496aa7d1450ee5"
   end
 
   depends_on "go" => :build
