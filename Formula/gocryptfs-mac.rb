44f43cfa378b2af795a63c24f7c83aef7cf7714d
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-03-20 14:19:29 +0000

    gocryptfs-mac: update 2.5.2 bottle.

--- Formula/gocryptfs-mac.rb
+++ Formula/gocryptfs-mac.rb
@@ -9,8 +9,8 @@ class GocryptfsMac < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/gromgit/fuse"
-    sha256 cellar: :any, arm64_sonoma: "5e46fef0657f37ac1990b1b83f4d8d1af06bf66735faa89983026bd115b00de1"
-    sha256 cellar: :any, ventura:      "b5afded46b2a7493da7923de46fe8ee7a7c2bca2f73047a466e9f861ffa40769"
+    sha256 cellar: :any, arm64_sonoma: "4abd9d34815e2858278ff38ff2eb30b7ddc1b12b94452d5294ad6b0e4be5f3e8"
+    sha256 cellar: :any, ventura:      "75331c2d30f380316d4817dcf0c5a667de2d9337239eb0a6c8a8a9ee6378270e"
   end
 
   depends_on "go" => :build
