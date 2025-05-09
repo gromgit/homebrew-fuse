fa84c2e82d4f5a04d575a70f5fd7dded3bd56022
gromgit <the.gromgit@gmail.com>
2025-05-09 18:38:07 +0000

    gocryptfs-mac 2.5.4
    Closes #120.
    
    Signed-off-by: Adrian Ho <215702+gromgit@users.noreply.github.com>

--- Formula/gocryptfs-mac.rb
+++ Formula/gocryptfs-mac.rb
@@ -3,8 +3,8 @@ require_relative "../require/macfuse"
 class GocryptfsMac < Formula
   desc "Encrypted overlay filesystem written in Go"
   homepage "https://nuetzlich.net/gocryptfs/"
-  url "https://github.com/rfjakob/gocryptfs/releases/download/v2.5.3/gocryptfs_v2.5.3_src-deps.tar.gz"
-  sha256 "4b6d874b5383be5ed33d7ef7a5a6152d2b6a5d1965215a426ec855c043138ee2"
+  url "https://github.com/rfjakob/gocryptfs/releases/download/v2.5.4/gocryptfs_v2.5.4_src-deps.tar.gz"
+  sha256 "0db47fe41f46d1ff5b3ff4f1cc1088ab324a95af03995348435dcc20a5ff0282"
   license "MIT"
 
   bottle do
