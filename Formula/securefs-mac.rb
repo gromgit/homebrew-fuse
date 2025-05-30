a5d6d5ad79625ef6f841f17ec7940072a551fa9d
gromgit <the.gromgit@gmail.com>
2025-05-30 14:33:19 +0000

    securefs-mac 1.1.1

--- Formula/securefs-mac.rb
+++ Formula/securefs-mac.rb
@@ -3,8 +3,8 @@ require_relative "../require/macfuse"
 class SecurefsMac < Formula
   desc "Filesystem with transparent authenticated encryption"
   homepage "https://github.com/netheril96/securefs"
-  url "https://github.com/netheril96/securefs/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "3b1d75c8716abafebd45466ddde33dba0ba93371d75ff2b8594e7822d29bd1f9"
+  url "https://github.com/netheril96/securefs/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "a4b0ceaaca98d25ed062bce0649bd43c83d5ea78d93d1fa4f227a2d59bfb7e62"
   license "MIT"
   head "https://github.com/netheril96/securefs.git", branch: "master"
 
