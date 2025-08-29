4d3675c7b06ef0ffc5045f18fccb66e38d6c74d3
gromgit <the.gromgit@gmail.com>
2025-08-29 22:29:03 +0000

    dwarfs-fuse-mac 0.13.0
    Closes #154.
    
    Signed-off-by: Adrian Ho <215702+gromgit@users.noreply.github.com>

--- Formula/dwarfs-fuse-mac.rb
+++ Formula/dwarfs-fuse-mac.rb
@@ -3,12 +3,13 @@ require_relative "../require/macfuse"
 class DwarfsFuseMac < Formula
   desc "Fast high compression read-only file system (macFUSE driver)"
   homepage "https://github.com/mhx/dwarfs"
-  url "https://github.com/mhx/dwarfs/releases/download/v0.12.4/dwarfs-0.12.4.tar.xz"
-  sha256 "352d13a3c7d9416e0a7d0d959306a25908b58d1ff47fb97e30a7c8490fcff259"
+  url "https://github.com/mhx/dwarfs/releases/download/v0.13.0/dwarfs-0.13.0.tar.xz"
+  sha256 "d0654fcc1219bfd11c96f737011d141c3ae5929620cd22928e49f25c37a15dc9"
   license "GPL-3.0-or-later"
 
   livecheck do
     url :stable
+    regex(/^(?:release[._-])?v?(\d+(?:\.\d+)+)$/i)
     strategy :github_latest
   end
 
@@ -19,33 +20,65 @@ class DwarfsFuseMac < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "dwarfs"
+  depends_on "googletest" => :build
+  depends_on "pkgconf" => :build
+  depends_on "boost"
+  depends_on "brotli"
+  depends_on "double-conversion"
+  depends_on "flac"
+  depends_on "fmt"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "howard-hinnant-date"
+  depends_on "libarchive"
+  depends_on "libevent"
+  depends_on "libsodium"
   depends_on "llvm" if DevelopmentTools.clang_build_version <= 1500
+  depends_on "lz4"
   depends_on MacfuseRequirement
   depends_on :macos
+  depends_on "nlohmann-json"
+  depends_on "openssl@3"
+  depends_on "parallel-hashmap"
+  depends_on "range-v3"
+  depends_on "utf8cpp"
+  depends_on "xxhash"
+  depends_on "xz"
+  depends_on "zstd"
+
+  conflicts_with "dwarfs", because: "both install the same binaries"
 
   fails_with :clang do
     build 1500
     cause "Not all required C++20 features are supported"
   end
 
+  # Workaround for Boost 1.89.0 until upstream Folly fix.
+  # Issue ref: https://github.com/facebook/folly/issues/2489
+  patch :DATA
+
   def install
     args = %W[
       -DBUILD_SHARED_LIBS=ON
       -DCMAKE_INSTALL_RPATH=#{rpath}
-      -DWITH_LIBDWARFS=OFF
-      -DWITH_TOOLS=OFF
+      -DWITH_LIBDWARFS=ON
+      -DWITH_TOOLS=ON
       -DWITH_FUSE_DRIVER=ON
-      -DWITH_TESTS=OFF
+      -DWITH_TESTS=ON
       -DWITH_MAN_PAGES=ON
       -DENABLE_PERFMON=ON
+      -DTRY_ENABLE_FLAC=ON
+      -DENABLE_RICEPP=ON
       -DENABLE_STACKTRACE=OFF
       -DDISABLE_CCACHE=ON
       -DDISABLE_MOLD=ON
+      -DPREFER_SYSTEM_GTEST=ON
     ]
 
     if DevelopmentTools.clang_build_version <= 1500
+      # No ASAN for folly
+      ENV.append "CXXFLAGS", "-D_LIBCPP_HAS_NO_ASAN"
+
       ENV.llvm_clang
 
       # Needed in order to find the C++ standard library
@@ -63,3 +96,25 @@ class DwarfsFuseMac < Formula
     system sbin/"dwarfs", "--help"
   end
 end
+
+__END__
+--- a/folly/CMake/folly-config.cmake.in
++++ b/folly/CMake/folly-config.cmake.in
+@@ -38,7 +38,6 @@ find_dependency(Boost 1.51.0 MODULE
+     filesystem
+     program_options
+     regex
+-    system
+     thread
+   REQUIRED
+ )
+--- a/folly/CMake/folly-deps.cmake
++++ b/folly/CMake/folly-deps.cmake
+@@ -41,7 +41,6 @@ find_package(Boost 1.51.0 MODULE
+     filesystem
+     program_options
+     regex
+-    system
+     thread
+   REQUIRED
+ )
