3680199b164f4b42752c8653900b694379c73402
gromgit <the.gromgit@gmail.com>
2026-02-16 19:01:22 +0000

    hfsfuse-mac 0.397

--- Formula/hfsfuse-mac.rb
+++ Formula/hfsfuse-mac.rb
@@ -3,8 +3,8 @@ require_relative "../require/macfuse"
 class HfsfuseMac < Formula
   desc "FUSE driver for HFS+ filesystems (read-only)"
   homepage "https://github.com/0x09/hfsfuse"
-  url "https://github.com/0x09/hfsfuse/releases/download/0.318/hfsfuse-0.318.tar.gz"
-  sha256 "01a0dff1fd83c25a88250e3b4ef150fe4eaa920db02770ca126c7da9fc404a1e"
+  url "https://github.com/0x09/hfsfuse/releases/download/0.397/hfsfuse-0.397.tar.gz"
+  sha256 "06201fc07680f6ecbdd4d0091bdc949ef88fe2c2b204bf377328a5a2b7fd45d8"
   license all_of: ["BSD-2-Clause", "MIT"]
   head "https://github.com/0x09/hfsfuse.git", branch: "master"
 
