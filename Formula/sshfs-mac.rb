114a94aead196b179002b700203452e1e2b2fd1e
Adrian Ho <215702+gromgit@users.noreply.github.com>
2026-07-16 10:58:16 +0000

    sshfs-mac: update 3.7.6 bottle.

--- Formula/sshfs-mac.rb
+++ Formula/sshfs-mac.rb
@@ -8,12 +8,9 @@ class SshfsMac < Formula
   license any_of: ["LGPL-2.1-only", "GPL-2.0-only"]
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-fuse/releases/download/sshfs-mac-2.10"
-    sha256 cellar: :any, arm64_monterey: "422abbbbd1804e67c27a21b976e4e82cfdffd977b5aa2cf7ee4e21fc2b548167"
-    sha256 cellar: :any, monterey:       "ffbc11b371d196dd6a32c13b21dfc28af7f7577145a62a165062228c2c661263"
-    sha256 cellar: :any, big_sur:        "9e021d24580ec55f4ab034dba0e264906d9db5c57036fe83710bc601bc6885c6"
-    sha256 cellar: :any, catalina:       "09f254420411218a784c783df760d2c652acd280eecc875d60e41014f80011cc"
-    sha256 cellar: :any, mojave:         "6389b69b921295f5be6eb35336649f558375eb24da60573b376a313331a4d18c"
+    root_url "https://ghcr.io/v2/gromgit/fuse"
+    sha256 cellar: :any, arm64_tahoe:   "114ec7f6cac5db3f7b4e4a63381f11958f3e20ea614eb9dda1356ed70ffbf4e5"
+    sha256 cellar: :any, arm64_sequoia: "211b6ce7c0e73abcca42f646fda6ad8573ff10de6f846636f382f8258f35d562"
   end
 
   depends_on "coreutils" => :build
