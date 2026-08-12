15acf4f2ac43b029fafdcb1a4db08ebc80368467
gromgit <the.gromgit@gmail.com>
2026-08-12 04:16:14 +0000

    hfsfuse-mac 0.435
    Closes #285.
    
    Signed-off-by: Adrian Ho <215702+gromgit@users.noreply.github.com>

--- Formula/hfsfuse-mac.rb
+++ Formula/hfsfuse-mac.rb
@@ -3,8 +3,8 @@ require_relative "../require/macfuse"
 class HfsfuseMac < Formula
   desc "FUSE driver for HFS+ filesystems (read-only)"
   homepage "https://github.com/0x09/hfsfuse"
-  url "https://github.com/0x09/hfsfuse/releases/download/0.425/hfsfuse-0.425.tar.gz"
-  sha256 "1da352d9c0c4c22d314d789aeddb64358224c371c2bd295c0fe12fb82dde17ff"
+  url "https://github.com/0x09/hfsfuse/releases/download/0.435/hfsfuse-0.435.tar.gz"
+  sha256 "d0fcdee633c9f497ce17784c217795a952d79fb40d0d309c1ad0f52180505075"
   license all_of: ["BSD-2-Clause", "MIT"]
   head "https://github.com/0x09/hfsfuse.git", branch: "master"
 
